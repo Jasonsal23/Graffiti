7015d5bda06b773b44b8c465b12954b7e1abb85c
Jason <jason@Jasons-MacBook-Air.local>
2023-03-09 19:18:19 -0800

    DOM

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ for(let i=0; i<dogImages.length; i++){
 let dogName = document.querySelectorAll('.dog-name')
 for(let i=0; i<dogName.length; i++){
     dogName[i].style.textAlign= "left"
+    dogName[i].style.color= 'pink'
 }
 let footer = document.querySelector('.footer')
 footer.style.borderStyle ='solid'
